abcade00020e4b2e717b651e82320368cc81b705
Gil Gardosh <gilgardosh@gmail.com>
2020-12-02 10:35:31 +0200

    config test

--- packages/graphql-codegen-cli/tests/gil.spec.ts
+++ packages/graphql-codegen-cli/tests/gil.spec.ts
@@ -1,4 +1,5 @@
 import { TempDir } from './utils';
+<<<<<<< HEAD
 import { createContext, executeCodegen, parseArgv } from '../src';
 
 const mockConfig = (str: string, file = './config.yml') => {
@@ -10,6 +11,11 @@ const mockConfig = (str: string, file = './config.yml') => {
   }`
   );
 };
+=======
+import { createContext, parseArgv } from '../src/config';
+
+const mockConfig = (str: string, file = './codegen.yml') => temp.createFile(file, str);
+>>>>>>> a93dff02... config test
 const createArgv = (str = ''): string[] => {
   const result = ['node', 'fake.js'];
   const regexp = /([^\s'"]+(['"])([^\2]*?)\2)|[^\s'"]+|(['"])([^\4]*?)\4/gi;
@@ -43,6 +49,7 @@ describe('CLI Flags', () => {
   });
 
   it('Gil test', async () => {
+<<<<<<< HEAD
     mockConfig(
       `
       config:
@@ -74,4 +81,74 @@ describe('CLI Flags', () => {
     // expect(config).toEqual(config2);
     // expect(config2.schema[0]).toEqual('./gil-test.graphql');
   });
+=======
+    // mockConfig(`
+    // schema:
+    //   - './packages/modules/**/schema/**/*.gql'
+    // generates:
+    //   ./packages/core/src/generated/global.gql.types.generated.ts:
+    //     plugins:
+    //       - typescript
+    //       - typescript-operations
+    //       - typescript-resolvers
+    //       - add:
+    //           placement: prepend
+    //           content: "import { DeepPartial } from 'utility-types';"
+    //     config:
+    //       defaultMapper: DeepPartial<{T}>
+    //       federation: true
+    // watch: true
+    // overwrite: true
+    // `);
+    // const args = createArgv();
+    // const context = await createContext(parseArgv(args));
+    // const config = JSON.stringify(context.getConfig());
+    mockConfig(
+      `
+    projects:
+      globalgql:
+        config:
+          federation: true
+        schema:
+          - "./packages/**/gil-test.graphql"
+        extensions:
+          codegen:
+            generates:
+              ./packages/core/src/generated/global.gql.types.generated.ts:
+                plugins:
+                  - typescript
+                  - typescript-operations
+                  - typescript-resolvers
+                  - add:
+                      placement: prepend
+                      content: "import { DeepPartial } from 'utility-types';"
+                config:
+                  federation: true
+                  defaultMapper: DeepPartial<{T}>
+    `
+    );
+    const args2 = createArgv();
+    const context2 = await createContext(parseArgv(args2));
+    const config2 = context2.getConfig();
+    // expect(config).toEqual(config2);
+    expect(config2.schema).toEqual('gil-test.graphql');
+  });
+
+  // it('Should use different config file correctly with --config', async () => {
+  //   mockConfig(
+  //     `
+  //       schema: schema.graphql
+  //       generates:
+  //           file.ts:
+  //               - plugin
+  //     `,
+  //     'other.yml'
+  //   );
+  //   const args = createArgv('--config other.yml');
+  //   const context = await createContext(parseArgv(args));
+  //   const config = context.getConfig();
+  //   expect(config.schema).toEqual('schema.graphql');
+  //   expect(config.generates).toEqual({ 'file.ts': ['plugin'] });
+  // });
+>>>>>>> a93dff02... config test
 });
